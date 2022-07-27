458f0d6149af254e33ced426efc8f607632d41ac
Harsha0416 <88873514+Harsha0416@users.noreply.github.com>
2022-07-27 16:54:32 +0530

    Variables in java

---
+++ Variable.java
@@ -0,0 +1,14 @@
+// Write a program to Print details using variables ?
+class Variable 
+{
+	public static void main(String[] args) 
+	{
+		String a = "Virat";
+		System.out.println(a);
+		int b = 35;
+		System.out.println(b);
+		double c = 54.67;
+		System.out.println(c);
+
+	}
+}
